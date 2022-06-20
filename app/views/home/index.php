368a2d8423563acc6f5f23c0d606fcc407b5d098
Rizeto-AriefMarzuki <ariefzukik@gmail.com>
2022-06-20 22:07:37 +0700

    Tugas 2 PBWL 2022

---
+++ app/views/home/index.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="<?php echo AST; ?>/css/login.css">
+<div class="card">
+    <div class="card-header">
+        Selamat Datang
+    </div>
+    <div class="card-body">
+        Selamat Datang di Tugas 2 Pemrograman Berbasis Web Lanjutan tahun 2022
+        <p></p>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="<?php echo AST; ?>/images/arip.jpeg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Arief Marzuki - 0702193175</h5>
+                <p class="card-text justify">Saya adalah seorang mahasiswa dari Universitas Islam Negeri Sumatera Utara saat Jurusan Sistem Informasi, saat ini saya berada di Semester 6.</p>
+                <a href="#" class="btn btn-primary">Selengkapnya</a>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <img class="card-img-top" src="<?php echo AST; ?>/images/github.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Akun Github</h5>
+            <p class="card-text">Akun yang digunakan untuk mengumpulkan Tugas 2 Pemrograman Berbasis Web Lanjutan</p>
+            <p class="card-text"><small class="text-muted">Kelas SI-3</small></p>
+        </div>
+    </div>
+</div>
+<script src="<?php echo AST; ?>/js/bootstrap.min.js"></script>
+</div>
